1178d7a90fa1ab3b9fbab8425f754f2e13e92111
kabosuMy3a <my3a3380@gmail.com>
2022-11-22 16:04:33 +0900

    https 키 설정

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 import express from "express";
-import http from "http" ;
 import https from "https";
+import fs from 'fs';
 
 const app = express();
-const server = http.createServer(app);
+
+const options = {
+        ca: fs.readFileSync('fullchain.pem'),
+        key: fs.readFileSync('privkey.pem'),
+        cert: fs.readFileSync('cert.pem'),
+}
+
+const server = https.createServer(options, app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
 
 app.get('/',(req, res) => {
   res.send('hi');
