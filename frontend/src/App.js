08458dca3d38f529967a288f5bc036b925d617bb
kabosumy3a <my3a3380@gmail.com>
2022-11-22 23:58:45 +0900

    getUserMedia getDisplayMedia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import {useEffect, useRef} from 'react';
 
 function App() {
+  const localVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
+  const videoOptions = ['default','browser','window','monitor'] ;
+  const options = {audio: true, video: true} ;
+
+  useEffect(()=>{
+    const getLocalVideo = async () => {
+      options.video = {displaySurface: "browser"};
+      localVideoRef.current.srcObject = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
+      remoteVideoRef.current.srcObject = await navigator.mediaDevices.getDisplayMedia(options);
+    }
+    getLocalVideo();
+    localVideoRef.current.onloadedmetadata = () => {
+      localVideoRef.current.play();
+    }
+
+    remoteVideoRef.current.onloadedmetadata = () =>{
+      remoteVideoRef.current.play();
+    }
+
+  },[]);
+
+
+
   return (
-    <div> a </div>
-  );
+    <Layout>
+      <span>
+        <LocalVideo ref={localVideoRef} muted/>
+      </span>
+      <LocalVideo ref={remoteVideoRef}/>
+    </Layout>
+  ) ;
 }
 
-export default App;
+export default App ;
+
+const Layout = styled.div`
+  display: flex ;
+  flex-direction: column ;
+`;
+
+const LocalVideo = styled.video`
+  background-color: chartreuse;
+  width: 360px;
+  height: 360px;
+` ;
